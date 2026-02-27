de1ecf563e5fe2c3d8b418d4de240a05470ed798
androgodot <168885773+x1cOfficial@users.noreply.github.com>
2026-02-27 23:37:48 +1100

    fix again

--- Assets/SideSource.js
+++ Assets/SideSource.js
@@ -5,7 +5,7 @@ const isIOS = () => {
 const newElement = document.createElement('div');
 if (isIOS()) {
   newElement.innerHTML = '<a href="sidestore://source?url=https://x1cofficial.github.io/SideStore/apps.json" style="display: inline-block;"><img src="https://x1cOfficial.github.io/SideStore/Assets/sidebadge.png" alt="Get on SideStore" style="width: 246px; height: 82px; vertical-align: middle; object-fit: contain;" /></a><a href="altstore://source?url=https://x1cofficial.github.io/SideStore/apps.json" style="display: inline-block;"><img src="https://x1cOfficial.github.io/SideStore/Assets/altbadge.png" alt="Get on AltStore" style="width: 246px; height: 82px; vertical-align: middle; object-fit: contain;" /></a>';
-} else {
+else {
   newElement.innerHTML = '<a href="https://altsource.by.lao.sb/browse/?source=https%3A%2F%2Fx1cOfficial.github.io%2FSideStore%2Fapps.json" style="display: inline-block;"><img src="https://x1cOfficial.github.io/SideStore/Assets/sidebadge.png" alt="Get on SideStore" style="width: 246px; height: 82px; vertical-align: middle; object-fit: contain;" /></a><a href="https://altsource.by.lao.sb/browse/?source=https%3A%2F%2Fx1cOfficial.github.io%2FSideStore%2Fapps.json" style="display: inline-block;"><img src="https://x1cOfficial.github.io/SideStore/Assets/altbadge.png" alt="Get on AltStore" style="width: 246px; height: 82px; vertical-align: middle; object-fit: contain;" /></a>';
 }
 parentContainer.appendChild(newElement);
