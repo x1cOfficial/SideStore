bc3e725f7cc3dbea2ffa713b5d6536bc07192dbc
androgodot <168885773+x1cOfficial@users.noreply.github.com>
2026-02-27 23:15:08 +1100

    Update SideSource.js

--- Assets/SideSource.js
+++ Assets/SideSource.js
@@ -1,15 +1,13 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent) || 
-                (navigator.platform === 'MacIntel' && navigator.maxTouchPoints > 1);
-
-  const container = document.getElementById('content-container');
-  const newElement = document.createElement('div');
-
-  if (isIOS) {
-    newElement.innerHTML = '<a href="https://altsource.by.lao.sb/browse/?source=https%3A%2F%2Fx1cOfficial.github.io%2FSideStore%2Fapps.json" style="display: inline-block;"><img src="https://x1cOfficial.github.io/SideStore/Assets/sidebadge.png" alt="Get on SideStore" style="width: 246px; height: 82px; vertical-align: middle; object-fit: contain;" /></a>';
-  } else {
-    newElement.innerHTML = '<a href="sidestore://source?url=https://x1cofficial.github.io/SideStore/apps.json" style="display: inline-block;"><img src="https://x1cOfficial.github.io/SideStore/Assets/sidebadge.png" alt="Get on SideStore" style="width: 246px; height: 82px; vertical-align: middle; object-fit: contain;" /></a>';
-  }
-
-  container.appendChild(newElement);
-});
+const parentContainer = document.currentScript.parentElement;
+const isIOS = () => {
+  return /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
+};
+const newElement = document.createElement('div');
+if (isIOS()) {
+  newElement.innerHTML = '<a href="sidestore://source?url=https://x1cofficial.github.io/SideStore/apps.json" style="display: inline-block;"><img src="https://x1cOfficial.github.io/SideStore/Assets/sidebadge.png" alt="Get on SideStore" style="width: 246px; height: 82px; vertical-align: middle; object-fit: contain;" /></a>';
+  newElement.style.color = 'blue';
+} else {
+  newElement.innerHTML = '<a href="https://altsource.by.lao.sb/browse/?source=https%3A%2F%2Fx1cOfficial.github.io%2FSideStore%2Fapps.json" style="display: inline-block;"><img src="https://x1cOfficial.github.io/SideStore/Assets/sidebadge.png" alt="Get on SideStore" style="width: 246px; height: 82px; vertical-align: middle; object-fit: contain;" /></a>';
+  newElement.style.color = 'green';
+}
+parentContainer.appendChild(newElement);
