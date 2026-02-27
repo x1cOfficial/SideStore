917843930068edbcb6a72e70cfe9045917944b31
androgodot <168885773+x1cOfficial@users.noreply.github.com>
2026-02-27 23:27:26 +1100

    Update SideSource.js

--- Assets/SideSource.js
+++ Assets/SideSource.js
@@ -4,7 +4,7 @@ const isIOS = () => {
 };
 const newElement = document.createElement('div');
 if (isIOS()) {
-  newElement.innerHTML = '<a href="sidestore://source?url=https://x1cofficial.github.io/SideStore/apps.json" style="display: inline-block;"><img src="https://x1cOfficial.github.io/SideStore/Assets/sidebadge.png" alt="Get on SideStore" style="width: 246px; height: 82px; vertical-align: middle; object-fit: contain;" /></a>';
+  newElement.innerHTML = '<script src="addsource.js"></script><a href="javascript:void(0)" style="display: inline-block;onclick="openStoreSources(); return false;">"><img src="https://x1cOfficial.github.io/SideStore/Assets/sidebadge.png" alt="Get on SideStore" style="width: 246px; height: 82px; vertical-align: middle; object-fit: contain;" /></a>';
   newElement.style.color = 'blue';
 } else {
   newElement.innerHTML = '<a href="https://altsource.by.lao.sb/browse/?source=https%3A%2F%2Fx1cOfficial.github.io%2FSideStore%2Fapps.json" style="display: inline-block;"><img src="https://x1cOfficial.github.io/SideStore/Assets/sidebadge.png" alt="Get on SideStore" style="width: 246px; height: 82px; vertical-align: middle; object-fit: contain;" /></a>';
