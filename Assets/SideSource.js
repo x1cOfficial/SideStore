a985ee00018f597721bf59adb7173ca383218f06
androgodot <168885773+x1cOfficial@users.noreply.github.com>
2026-02-27 23:34:34 +1100

    revert changes

--- Assets/SideSource.js
+++ Assets/SideSource.js
@@ -4,8 +4,9 @@ const isIOS = () => {
 };
 const newElement = document.createElement('div');
 if (isIOS()) {
-  newElement.innerHTML = '<script src="https://x1cOfficial.github.io/SideStore/Assets/addsource.js"></script><a href="javascript:void(0)" style="display: inline-block;onclick="openStoreSources(); return false;">"><img src="https://x1cOfficial.github.io/SideStore/Assets/sidebadge.png" alt="Get on SideStore" style="width: 246px; height: 82px; vertical-align: middle; object-fit: contain;" /></a>';
+  newElement.innerHTML = '<a href="sidestore://source?url=https://x1cofficial.github.io/SideStore/apps.json" style="display: inline-block;"><img src="https://x1cOfficial.github.io/SideStore/Assets/sidebadge.png" alt="Get on SideStore" style="width: 246px; height: 82px; vertical-align: middle; object-fit: contain;" /></a><a href="altstore://source?url=https://x1cofficial.github.io/SideStore/apps.json" style="display: inline-block;"><img src="https://x1cOfficial.github.io/SideStore/Assets/altbadge.png" alt="Get on AltStore" style="width: 246px; height: 82px; vertical-align: middle; object-fit: contain;" /></a>';
+}
 } else {
-  newElement.innerHTML = '<a href="https://altsource.by.lao.sb/browse/?source=https%3A%2F%2Fx1cOfficial.github.io%2FSideStore%2Fapps.json" style="display: inline-block;"><img src="https://x1cOfficial.github.io/SideStore/Assets/sidebadge.png" alt="Get on SideStore" style="width: 246px; height: 82px; vertical-align: middle; object-fit: contain;" /></a>';
+  newElement.innerHTML = '<a href="https://altsource.by.lao.sb/browse/?source=https%3A%2F%2Fx1cOfficial.github.io%2FSideStore%2Fapps.json" style="display: inline-block;"><img src="https://x1cOfficial.github.io/SideStore/Assets/sidebadge.png" alt="Get on SideStore" style="width: 246px; height: 82px; vertical-align: middle; object-fit: contain;" /></a><a href="https://altsource.by.lao.sb/browse/?source=https%3A%2F%2Fx1cOfficial.github.io%2FSideStore%2Fapps.json" style="display: inline-block;"><img src="https://x1cOfficial.github.io/SideStore/Assets/altbadge.png" alt="Get on AltStore" style="width: 246px; height: 82px; vertical-align: middle; object-fit: contain;" /></a>';
 }
 parentContainer.appendChild(newElement);
